0c4f115691ecee93740133c64af9f27d84d4c0ab
LAPTOP-HCATFVOS\jua-d <juan.david.208080@gmail.com>
2021-03-12 21:39:19 -0500

    adding new views and some code to make them work properly

--- personalPortfolio-project/jobs/models.py
+++ personalPortfolio-project/jobs/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Job (models.Model):
+    tittle = models.CharField(max_length=10)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.tittle
